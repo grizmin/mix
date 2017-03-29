517835f7125125c112117b3219d214742d119bc9
Konstantin Krastev <grizmin@gmail.com>
2017-03-29 23:05:31 +0300

    added marshrut class

--- zhod.py
+++ zhod.py
@@ -1,11 +1,13 @@
-class Car():
+import math
 
+class Car():
     valuta = 'leva'
     fuelType = {'petrol': 2.08,'diesel': 1.75}
 
-    def __init__(self, literkm, fuel_type):
+    def __init__(self, literkm, name, fuel_type):
         self.fuel_type = fuel_type
         self.literkm = literkm
+        self.name = name
 
     @property
     def fuel_type(self):
@@ -21,35 +23,57 @@ class Car():
     def fuelPrice(self):
         return Car.fuelType[self.fuel_type]
 
-    def razhod(self, km):
+    @property
+    def whoami(self):
         """
-        :param km: (int) Kilometers
-        :return: (float) price for the kilometers passed
+        Name of the car object
+        :return: (string) name
         """
-        cena = (self.literkm/100) * km * self.fuelPrice
-        print(cena, Car.valuta)
-        return cena
+        return self.name
 
 class marshrut():
-    def __init__(self):
-        self.cars = []
+    def __init__(self, km, **kwargs):
+        self.cars = kwargs.get('cars', [])
+        self.averagespeed = kwargs.get('speed', 50)
+        self.km = km
 
     def addCar(self, car):
         self.cars.append(car)
 
     def getCars(self):
-        print([i for i in enumerate(self.cars)])
+        """
+        :return: Cars asigned to this marshrut
+        """
+        print('\n'.join('{}: {}'.format(k, v.whoami) for k, v in enumerate(self.cars)))
 
-audi = Car(7.3, 'diesel')
-citroen = Car(9.5, 'diesel')
+    def razhod(self):
+        """
+        :param km: (int) Kilometers
+        :return: (float) price for the kilometers passed
+        """
+        for car in self.cars:
+            cena = (car.literkm/100) * self.km * car.fuelPrice
+            print("{} struva {:.3}".format(car.whoami, cena), Car.valuta)
+        return cena
 
-p1 = Car(7, 'diesel')
-p2 = Car(9, 'diesel')
+    def vreme(self):
+        for car in self.cars:
+            vreme = (self.km / self.averagespeed)
+            ftime = "{:.0f}h{:.0f}m".format(vreme, math.modf(vreme)[0] * 60)
+            print("{} minava marshruta za {}".format(car.whoami, ftime))
+        return vreme
 
+audi1 = Car(7.5, 'A6 Okolovrystno', 'diesel')
+audi2 = Car(11, 'A6 Center', 'diesel')
 
-p1.razhod(26)
-p2.razhod(16)
+# prez centera za 17km
+prez_center = marshrut(17, speed = 35)
+prez_center.addCar(audi2)
+prez_center.razhod()
+prez_center.vreme()
 
-m1 = marshrut()
-m1.addCar(p1)
-m1.getCars()
+# ringroad za 28km
+ringroad = marshrut(27, speed = 70)
+ringroad.addCar(audi1)
+ringroad.razhod()
+ringroad.vreme()
